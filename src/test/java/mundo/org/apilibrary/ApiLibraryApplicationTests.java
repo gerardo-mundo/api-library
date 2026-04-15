446d93fa07e84e9a1bd4b7d49ac2a74438a9d4f9
Gerardo Mundo <ger.mundo66@gmail.com>
2026-04-15 00:45:19 -0600

    Initial commit

---
+++ src/test/java/mundo/org/apilibrary/ApiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package mundo.org.apilibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
