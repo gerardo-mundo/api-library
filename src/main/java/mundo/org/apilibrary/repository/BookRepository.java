0cd0352ca35782ae5a208e0ed4b86ff706f20499
Gerardo Mundo <ger.mundo66@gmail.com>
2026-04-16 16:14:49 -0600

    IMPL: BookRepository.java

---
+++ src/main/java/mundo/org/apilibrary/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package mundo.org.apilibrary.repository;
+
+
+import mundo.org.apilibrary.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+    Optional<Book> findByIsbn(String isbn);
+    List<Book> findAllByAuthor(String author);
+}
