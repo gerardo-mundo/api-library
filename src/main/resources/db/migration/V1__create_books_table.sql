c245a73e16f3848c57e28f606ebc6bc12e60ff7c
Gerardo Mundo <ger.mundo66@gmail.com>
2026-04-28 21:52:32 -0600

    RFAC: Flyway V1 SQL schema
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

--- src/main/resources/db/migration/V1__create_books_table.sql
+++ src/main/resources/db/migration/V1__create_books_table.sql
@@ -3,17 +3,17 @@ CREATE TABLE books
     id           UUID         NOT NULL,
     title        VARCHAR(255) NOT NULL,
     author       VARCHAR(255) NOT NULL,
-    publisher    VARCHAR(255) NOT NULL,
-    collection   VARCHAR(255),
-    acquisition  INTEGER      NOT NULL,
-    isbn         VARCHAR(255) NOT NULL,
     available    BOOLEAN      NOT NULL,
     created_at   TIMESTAMP WITHOUT TIME ZONE,
     updated_at   TIMESTAMP WITHOUT TIME ZONE,
     author_two   VARCHAR(255),
     author_three VARCHAR(255),
+    publisher    VARCHAR(255) NOT NULL,
+    collection   VARCHAR(255) NOT NULL,
+    isbn         VARCHAR(13)  NOT NULL,
+    acquisition  INTEGER      NOT NULL,
     CONSTRAINT pk_books PRIMARY KEY (id)
 );
 
 ALTER TABLE books
-    ADD CONSTRAINT uc_books_isbn UNIQUE (isbn);
+    ADD CONSTRAINT uc_books_acquisition UNIQUE (acquisition);
