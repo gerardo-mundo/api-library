2daa4d8c555d3111e27ae8d271d14ee46f2b4431
Gerardo Mundo <ger.mundo66@gmail.com>
2026-04-29 21:47:10 -0600

    IMPL: PublicationRepository

---
+++ src/main/java/mundo/org/apilibrary/repository/PublicationRepository.java
@@ -0,0 +1,13 @@
+package mundo.org.apilibrary.repository;
+
+import mundo.org.apilibrary.entities.Publication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PublicationRepository extends JpaRepository<Publication, UUID> {
+    Optional<Publication> findPublicationByIssn(String issn);
+    List<Publication> getAllByPublisher(String publisher);
+}
