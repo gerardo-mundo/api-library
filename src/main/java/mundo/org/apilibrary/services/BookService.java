ae11d8e04bc5c64bd8912a425d321d5985dfd03d
Gerardo Mundo <ger.mundo66@gmail.com>
2026-05-15 19:06:20 -0600

    IMPL: BookService
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

---
+++ src/main/java/mundo/org/apilibrary/services/BookService.java
@@ -0,0 +1,66 @@
+package mundo.org.apilibrary.services;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+
+import mundo.org.apilibrary.books.BookCreationDTO;
+import mundo.org.apilibrary.entities.Book;
+import mundo.org.apilibrary.mapper.BookMapper;
+import mundo.org.apilibrary.repository.BookRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    public BookService(BookRepository bookRepository, BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> findAllBooks() { return bookRepository.findAll(); }
+
+    public Book findById(UUID id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found with ID: " + id));
+    }
+
+    @Transactional
+    public Book createBook(BookCreationDTO dto) {
+        if(bookRepository.existsByIsbn(dto.isbn())) {
+            throw new EntityExistsException("Book with ISBN " + dto.isbn() + " already exists");
+        }
+
+        var newBook = bookMapper.toEntity(dto);
+        return bookRepository.save(newBook);
+    }
+
+    @Transactional
+    public Book updateBook(UUID id, BookCreationDTO dto) {
+        var book = bookRepository.findById(id);
+
+        if(book.isEmpty()) {
+            throw new EntityNotFoundException("Book with ISBN " + dto.isbn() + " not found");
+        }
+
+        var bookUpdated = bookMapper.toEntity(dto);
+        return bookRepository.save(bookUpdated);
+    }
+
+    @Transactional
+    public void deleteBook(UUID id) {
+        var book = bookRepository.findById(id);
+
+        if(book.isEmpty()) {
+            throw new EntityNotFoundException("Book doesn't exist");
+        }
+
+        bookRepository.deleteById(id);
+    }
+}
