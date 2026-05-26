251bbb8e3ffec00d1676e22d1a95793f990c296f
Gerardo Mundo <ger.mundo66@gmail.com>
2026-05-25 22:09:35 -0600

    RFAC: Publication class moved
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

--- src/main/java/mundo/org/apilibrary/entities/Magazine.java
+++ src/main/java/mundo/org/apilibrary/entities/Magazine.java
@@ -2,6 +2,7 @@ package mundo.org.apilibrary.entities;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import mundo.org.apilibrary.classes.Publication;
 
 @Entity
 @DiscriminatorValue(value = "MAGAZINE")
