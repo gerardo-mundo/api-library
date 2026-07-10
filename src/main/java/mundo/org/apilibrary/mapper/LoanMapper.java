4b8081a5d9107334524e5d9046556484a3f84f49
Gerardo Mundo <ger.mundo66@gmail.com>
2026-07-09 21:11:51 -0600

    (IMPL): LoanMapper
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

---
+++ src/main/java/mundo/org/apilibrary/mapper/LoanMapper.java
@@ -0,0 +1,33 @@
+package mundo.org.apilibrary.mapper;
+
+import mundo.org.apilibrary.DTO.loans.LoanCreationDTO;
+import mundo.org.apilibrary.DTO.loans.LoanDTO;
+import mundo.org.apilibrary.DTO.loans.LoanUpdateDTO;
+import mundo.org.apilibrary.entities.Loan;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface LoanMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "approver", ignore = true)
+    @Mapping(target = "borrower", ignore = true)
+    @Mapping(target = "borrowDate", ignore = true)
+    @Mapping(target = "returnDate", ignore = true)
+    @Mapping(target = "borrowedBooks", ignore = true)
+    Loan toEntity(LoanCreationDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "borrowDate", ignore = true)
+    @Mapping(target = "approver", ignore = true)
+    @Mapping(target = "borrower", ignore = true)
+    @Mapping(target = "borrowedBooks", ignore = true)
+    void updateEntity(LoanUpdateDTO dto, @MappingTarget Loan loan);
+
+    LoanDTO toDTO(Loan loan);
+
+    List<LoanDTO> toListDTO(List<Loan> loans);
+}
