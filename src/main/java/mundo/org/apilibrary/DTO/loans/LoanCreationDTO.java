b11bbc8dba0255b5b1ee2a725a122eaedf6917a5
Gerardo Mundo <ger.mundo66@gmail.com>
2026-07-20 16:43:37 -0600

    (FIX): loan active and status properties must have default values
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

--- src/main/java/mundo/org/apilibrary/DTO/loans/LoanCreationDTO.java
+++ src/main/java/mundo/org/apilibrary/DTO/loans/LoanCreationDTO.java
@@ -3,7 +3,6 @@ package mundo.org.apilibrary.DTO.loans;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import mundo.org.apilibrary.enums.LoanStatus;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,11 +14,6 @@ public record LoanCreationDTO(
         @NotNull(message = "The borrower Id is required")
         UUID borrower,
         @NotEmpty(message = "You should add at least one book")
-        @Size(min = 1, max = 3, message = "The numbers of books must be between 1 and 3 books")
-        List<UUID> borrowedBooks,
-        @NotNull
-        boolean active,
-        @NotNull(message = "You should pick at least one status type")
-        LoanStatus status
+        @Size(min = 1, max = 3, message = "The numbers of books must be between 1 and 3 books") List<UUID> borrowedBooks
 ) {
 }
