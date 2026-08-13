ff86874cac71df7177bec30998d13e98e4fff2fc
Gerardo Mundo <ger.mundo66@gmail.com>
2026-08-12 20:27:23 -0600

    (IMPOL): JpaSpecificationExecutor
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

--- src/main/java/mundo/org/apilibrary/repository/BookRepository.java
+++ src/main/java/mundo/org/apilibrary/repository/BookRepository.java
@@ -3,12 +3,13 @@ package mundo.org.apilibrary.repository;
 
 import mundo.org.apilibrary.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface BookRepository extends JpaRepository<Book, UUID>, JpaSpecificationExecutor<Book> {
     Optional<Book> findByIsbn(String isbn);
     List<Book> findAllByAuthor(String author);
     boolean existsByIsbn(String isbn);
