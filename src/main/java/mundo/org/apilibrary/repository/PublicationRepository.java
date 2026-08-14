5b2faeff13ab39f74aee20aebcaae84dcc10af9e
Gerardo Mundo <ger.mundo66@gmail.com>
2026-08-13 19:04:07 -0600

    (IMPL): JpaSpecificationExecutor<Publication>
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

--- src/main/java/mundo/org/apilibrary/repository/PublicationRepository.java
+++ src/main/java/mundo/org/apilibrary/repository/PublicationRepository.java
@@ -2,12 +2,13 @@ package mundo.org.apilibrary.repository;
 
 import mundo.org.apilibrary.classes.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface PublicationRepository extends JpaRepository<Publication, UUID> {
+public interface PublicationRepository extends JpaRepository<Publication, UUID>, JpaSpecificationExecutor<Publication> {
     Optional<Publication> findPublicationByIssn(String issn);
 
     List<Publication> getAllByPublisherIgnoreCase(String publisher);
