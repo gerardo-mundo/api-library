70058798a2fcaf9ac78c7cbcd9aa3715815844aa
Gerardo Mundo <ger.mundo66@gmail.com>
2026-08-14 18:17:35 -0600

    (IMPL): JpaSpecificationExecutor<User>
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

--- src/main/java/mundo/org/apilibrary/repository/UserRepository.java
+++ src/main/java/mundo/org/apilibrary/repository/UserRepository.java
@@ -3,12 +3,13 @@ package mundo.org.apilibrary.repository;
 import mundo.org.apilibrary.classes.User;
 import mundo.org.apilibrary.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
     Optional<User> findByEmail(String email);
     List<User> findByIsActive(boolean isActive);
 
