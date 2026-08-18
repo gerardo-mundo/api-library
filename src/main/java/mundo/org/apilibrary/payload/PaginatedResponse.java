891e4ed7715db69f2ea8cb79a3b4c1cbed6c50f9
Gerardo Mundo <ger.mundo66@gmail.com>
2026-08-17 19:11:05 -0600

    (IMPL): PaginatedResponse
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

---
+++ src/main/java/mundo/org/apilibrary/payload/PaginatedResponse.java
@@ -0,0 +1,25 @@
+package mundo.org.apilibrary.payload;
+
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public record PaginatedResponse<T>(
+        List<T> content,
+        int pageNumber,
+        int pageSize,
+        long totalElements,
+        int totalPages,
+        boolean isLast
+) {
+    public PaginatedResponse(Page<T> page) {
+        this(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isLast()
+        );
+    }
+}
