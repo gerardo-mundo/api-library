ee1f4bd50c2e2d497f5ba394deef3b1519a70d70
Gerardo Mundo <ger.mundo66@gmail.com>
2026-08-19 17:35:31 -0600

    (FIX): CI tests are not started due to inexistent Flyway migration
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

--- src/test/java/mundo/org/apilibrary/ApiLibraryApplicationTests.java
+++ src/test/java/mundo/org/apilibrary/ApiLibraryApplicationTests.java
@@ -2,8 +2,10 @@ package mundo.org.apilibrary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.flyway.enabled=true")
+@ActiveProfiles("dev")
 class ApiLibraryApplicationTests {
 
     @Test
