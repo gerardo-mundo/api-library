09af75189f23c5d25da81c283d7d6235e455903b
Gerardo Mundo <ger.mundo66@gmail.com>
2026-09-23 18:56:47 -0600

    (IMPL): AuthenticationServiceException
    Signed-off-by: Gerardo Mundo <ger.mundo66@gmail.com>

--- src/main/java/mundo/org/apilibrary/exceptions/GlobalExceptionHandler.java
+++ src/main/java/mundo/org/apilibrary/exceptions/GlobalExceptionHandler.java
@@ -2,14 +2,12 @@ package mundo.org.apilibrary.exceptions;
 
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
-
 import mundo.org.apilibrary.payload.ApiResponse;
-
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.validation.FieldError;
-
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -47,6 +45,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = IllegalAccessException.class)
     public ResponseEntity<ApiResponse<Object>> handleEntityIllegalEntityException(IllegalAccessException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.failure(ex.getMessage()));
+    }
+
+    @ExceptionHandler(value = AuthenticationServiceException.class)
+    public ResponseEntity<ApiResponse<Object>> handleAuthenticationServiceException(AuthenticationServiceException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.failure(ex.getMessage()));
     }
 
     @ExceptionHandler(value = Exception.class)
